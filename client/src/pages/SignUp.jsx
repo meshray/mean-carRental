352ecf72679ee13bbe292f842533ce6105f1293f
meshray <shrayrathore777@gmail.com>
2024-02-10 12:29:17 +0530

    changes in functionality of sign up & in

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
@@ -40,34 +40,35 @@ export default function SignUp() {
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
-       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-       <form  className='flex flex-col gap-4'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='text'
           placeholder='username'
           className='border p-3 rounded-lg'
           id='username'
-          
+          onChange={handleChange}
         />
         <input
           type='email'
           placeholder='email'
           className='border p-3 rounded-lg'
           id='email'
-                  
+          onChange={handleChange}
         />
         <input
           type='password'
           placeholder='password'
           className='border p-3 rounded-lg'
           id='password'
-                    
+          onChange={handleChange}
         />
 
         <button
+          disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          Sign Up
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
       <div className='flex gap-2 mt-5'>
@@ -78,5 +79,5 @@ export default function SignUp() {
       </div>
       {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-  )
+  );
 }
